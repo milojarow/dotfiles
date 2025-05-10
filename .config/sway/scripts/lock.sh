028499ff8df0b4ae75cdc1df0a4a60ff7a70d39b
milojarow <ahuja@hush.com>
2025-05-10 15:31:38 -0500

    adjusted lock screen functionality and added keybinding super+x to lock

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -x "$(command -v gtklock)" ]; then
-    gtklock --daemonize --follow-focus --idle-hide --start-hidden
-elif [ -x "$(command -v waylock)" ]; then
-    waylock -fork-on-lock
-elif [ -x "$(command -v swaylock)" ]; then
-    swaylock --daemonize --show-failed-attempts
+# Usar siempre el mismo nombre de archivo para sobrescribir la imagen anterior
+SCREENSHOT="/tmp/swaylock-blur.png"
+
+# Captura la pantalla
+grim "$SCREENSHOT"
+
+# Detecta qué comando usar para ImageMagick
+if command -v magick &> /dev/null; then
+    # Para ImageMagick 7 - aumentado el nivel de desenfoque de 8 a 15
+    magick "$SCREENSHOT" -blur 0x15 "$SCREENSHOT"
+else
+    # Para ImageMagick 6 o anterior - aumentado el nivel de desenfoque de 8 a 15
+    convert "$SCREENSHOT" -blur 0x15 "$SCREENSHOT"
 fi
+
+# Bloquea la pantalla con la imagen desenfocada
+swaylock -i "$SCREENSHOT"
+
+# Elimina la captura cuando termine swaylock
+rm "$SCREENSHOT"
