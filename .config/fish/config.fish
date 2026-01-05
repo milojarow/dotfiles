34cc0b856ea43c9e72114f0010ab92536958ad68
milojarow <ahuja@hush.com>
2026-01-05 17:48:22 -0600

    Show full directory path in terminal title
    Replace prompt_pwd with pwd for waybar display clarity.
    Previously: ~/p/web-blindando
    Now: ~/projects/web-blindando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -68,7 +68,8 @@ function fish_title
 
     # Include shell PID at the end for waybar to map window -> shell -> CWD
     # Format: "~/path [PID]" or "~/path: command - command [PID]"
-    echo (prompt_pwd)$command_part" [%self]"
+    # Use full path instead of abbreviated prompt_pwd for waybar clarity
+    echo (pwd | sed "s|^$HOME|~|")$command_part" [%self]"
 end
 
 
